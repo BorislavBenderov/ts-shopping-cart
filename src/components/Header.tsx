9bf03d324b6a972e7b9357e8c67e8a83d18db3cc
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-15 10:15:39 +0200

    Pass props type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
-export const Header = () => {
-  return (
-    <div>Header</div>
+type PropsType = {
+  viewCart: boolean;
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const Header = ({ viewCart, setViewCart }: PropsType) => {
+  const content = (
+    <header className="header">
+      <div className="header__title-bar">
+        <h1>Name</h1>
+        <div className="header__price-box">
+          <p>Total Items:</p>
+          <p>Total Price:</p>
+        </div>
+      </div>
+    </header>
   )
-}
+  return <div>Header</div>;
+};
