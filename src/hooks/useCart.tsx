ef1d3e4c25d6ff687d37a95dafe1aa64c31f058e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-15 11:01:39 +0200

    Add custom hook for cart context

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import { CartContext, UseCartContextType } from "../context/CartContext";
+
+export const useCart = (): UseCartContextType => {
+  return useContext(CartContext);
+};
